76d25f2dc6335d49679fcbfe33a846ab1748d553
Bernardo AS <fmacuacua116@gmail.com>
2026-05-30 09:50:49 -0300

    updated

--- SEMANA 8 JavasCript/aula 4/script.js
+++ SEMANA 8 JavasCript/aula 4/script.js
@@ -1,6 +1,6 @@
 function calcular(){
-    const Peso = document.getElementById('peso').value
-    const Altura = document.getElementById('altura').value
+    const Peso = document.getElementById('peso').value          /* usamos o value para dizer que essa constante sera um valor */
+    const Altura = document.getElementById('altura').value      /* usamos o value para dizer que essa constante sera um valor */
 
     const IMC  =   Peso/(Altura*Altura)
 
