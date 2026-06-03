da2ba09ef9902ceb081edab9c835532231c770c8
Bernardo AS <fmacuacua116@gmail.com>
2026-06-03 17:48:36 -0300

    updated

--- SEMANA 8 JavasCript/aula 7/calculadora/javascript.js
+++ SEMANA 8 JavasCript/aula 7/calculadora/javascript.js
@@ -1,19 +1,22 @@
 function calcular(operador){
 
-    let num1=document.getElementById('txt_num1').value
-    let num2=document.getElementById('txt_num2').value
+    const num1=document.getElementById('txt_num1').value
+    const num2=document.getElementById('txt_num2').value
 
     let lbl_res = document.getElementById('resultado')
 
     if(operador==='+'){
         let res=num1+num2
         lbl_res.innerHTML=`O resultado sera:` + res
+
     }else if(operador==='-'){
         let res=num1-num2
         lbl_res.innerHTML=`O resultado sera:` + res
+
     }else if(operador==='/'){
         let res=num1/num2
         lbl_res.innerHTML=`O resultado sera:` + res
+
     }else if(operador==='*'){
         let res=num1*num2
         lbl_res.innerHTML=`O resultado sera:` + res
