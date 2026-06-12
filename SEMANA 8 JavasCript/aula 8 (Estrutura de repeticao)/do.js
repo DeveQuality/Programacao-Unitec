f37ac2192f8aa0d1fe530bd8e7e97062c35a03c9
Bernardo AS <fmacuacua116@gmail.com>
2026-06-12 15:49:26 -0300

    updated

---
+++ SEMANA 8 JavasCript/aula 8 (Estrutura de repeticao)/do.js
@@ -0,0 +1,7 @@
+let i = 0;                      /* declaracao de variavel */
+
+do{
+    console.log('i = ' +i)      /* instrucao */
+    i++;                        /* controle */
+
+}while(i<=5)                    /* condicao */
---
+++ SEMANA 8 JavasCript/aula 8 (Estrutura de repeticao)/while.js
@@ -0,0 +1,6 @@
+let i = 0                           /* declaracao da variavel */
+
+while(i<=5){                        /* condicao */
+    console.log('i = '+i)           /* instrucao */
+    i++;                            /* controle */
+}
