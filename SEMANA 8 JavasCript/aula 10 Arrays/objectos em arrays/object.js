5e822ec255a780e8e844d36101ae6ad41ae85705
Bernardo AS <fmacuacua116@gmail.com>
2026-06-19 13:48:52 -0300

    updated

---
+++ SEMANA 8 JavasCript/aula 10 Arrays/objectos em arrays/object.js
@@ -0,0 +1,8 @@
+let frutas = [
+            {nome:'Banana', cor:'amarelo'},
+            {nome:'Maca', cor:'vermelha'},
+            {nome:'Pera', cor:'verde'},
+            {nome:'Morango', cor:'vermelha'}
+]
+
+console.log(frutas[3].nome)
