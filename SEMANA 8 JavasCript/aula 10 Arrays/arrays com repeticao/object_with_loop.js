d872f75839029970a9671b7c46c9943d92325169
Bernardo AS <fmacuacua116@gmail.com>
2026-06-21 14:11:51 -0300

    updated

---
+++ SEMANA 8 JavasCript/aula 10 Arrays/arrays com repeticao/object_with_loop.js
@@ -0,0 +1,10 @@
+let carro = [
+    {marca:'toyota' , modelo:'corola', matricula:'ABS123MC'},
+    {marca:'Mazda' , modelo:'demio', matricula:'SAB123MC'}
+]
+
+for(let i=0 ; i<carro.length ; i++){
+
+    console.log(carro[i].marca + '  ' + carro[i].modelo + '  ' + carro[i].matricula)
+
+}
